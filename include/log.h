ea8796047651108b63eefc4c1a61fbed1ea2b046
AlxirAnwar <2726753245@qq.com>
2022-04-26 23:35:23 +0800

    more info about log

--- include/log.h
+++ include/log.h
@@ -6,31 +6,46 @@
 #include <time.h>
 
 #define SIZE_OF_TIMEBUF 21
-
+// 输出到终端的日志，可通过注释此宏定义来取消终端日志（终端消息）
 // the choise of log to termoinal 
 #define Log_to_terminal
 
+// 输出到文件的日志，可通过注释此宏定义来取消文件日志
 // the choise of log to file
 #define Log_to_file
 
+// 日志文件
 FILE * log;
+// 日志文件名称
 char filename[32];
+
+// 初始化日志和结束日志
 int log_init();
 void log_close();
 
+// 获取当前系统时间，并以 YYYY-MM-DD HH:MM:SS 的形式放到curtime 串中，用于在日志中输出
 char * gettime();
 char curtime[21];
+
+// 客户端在连接的用户的ip存于此变量中，获取ic的代码在server文件中
 char cli_ip[32];
 
 
 // 方便调试 , to terminal 
 #ifdef Log_to_terminal
+// 输出到终端的日志信息，所以是stdout，标准输出
+// \33[1;32m ~~~ \33[0m  定义在终端中输出的颜色
+// __FILE__ 当前文件 
+// __LINE__ 当前代码所在行（原文件中）
+// __func__ 当前代码所在函数
+//  fromat,... 和   fromat ## __VA_ARGS__ 配套使用，会在format 位置填充 用户在使用该宏定义时的任意输出数据以及形式
 #define Log(format, ...) \
 	do { \
 		fprintf(stdout, "\33[1;32m[%s,%d,%s] " format "\33[0m\n", \
 				__FILE__, __LINE__, __func__, ## __VA_ARGS__); \
     } while(0)
 
+//  跟楼上的Log 唯一的在于颜色，error 日志输出为红色
 #define Log_er(format, ...) \
 	do { \
 		fprintf(stdout, "\33[1;31m[%s,%d,%s] " format "\33[0m\n", \
@@ -43,12 +58,21 @@ char cli_ip[32];
 
 // Access LOg and Error Log , to file
 #ifdef Log_to_file
+// 该宏定义日志方法来自nemu 源码
+// 先打开log文件，再把日志信息输出到文件中，再关闭文件，为一次日志的完整执行
+// 输出形式按照apache 的日志给出 如下
+// 127.0.0.1 - [2022-04-26 13:50:32] "BAD REQUEST" 400 28
+// cli_ip 以在sercer 代码中获取并放到 cli_ip 串中
+// gettime() 获取时间在log.c 文件中包装好了
+// 余下三个信息为当前request 的mothod , uri 以及 http_version
+// 最后两个威为 当前respond 的代码 和 返回消息的长度
 #define Acceess_Log(a,c,l)\
 	do{\
 		log=fopen(filename,"a+");\
 		fprintf(log,"%s - [%s] \"%s %s %s\" %d %d\n",cli_ip,gettime(),a->http_method,a->http_uri,a->http_version,c,l);\
 		fclose(log);\
 	}while(0)
+// 当bad request 时，因为ruquest 结构体为空，故不能通过传进来request 来输出信息，续作特殊处理
 #define Acceess_Log_Null(c,l)\
 	do{\
 		log=fopen(filename,"a+");\
--- src/log.c
+++ src/log.c
@@ -1,7 +1,11 @@
 #include "log.h"
 
+// 输出日日志的文件，全局变量，在log.h 中
 extern FILE *log;
 
+// 初始化日志，所谓初始化，也就是创建/清空输出日志的文件
+// 如果第一次打开文件未成功，就返回初始化失败
+// 第一次能成功打开，后续用宏定义输出日志时也就不用担心打不开的问题了（可能不带太严谨）
 int log_init(){
     strcpy(filename,"log.txt");
     log=fopen(filename,"w+");
@@ -18,10 +22,16 @@ int log_init(){
     fclose(log);
 }
 
+
+// 最后一次确认关闭打开的文件
 void log_close(){
     fclose(log);
 }
 
+
+// 获取当前时间
+// 并转化成YYYY-MM-DD HH:MM:SS 的形式
+// 放到全局变量 certime 中（此变量专门用来存当前时刻，因为经常使用，所以在log.h 文件中定义成了全局变量）
 char * gettime(){
     memset(curtime,0,sizeof(curtime));
     time_t ltime;
